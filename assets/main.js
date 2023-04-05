427e40f8889db8caef174dc4e9d23967b34eb0f4
spaceinvader10 <54293311+spaceinvader10@users.noreply.github.com>
2023-04-05 13:13:56 -0700

    Removed decimal point as it seperate Else if and integrated it with the event listener KeyDown. This allowed for decimal adding with checking for aNum and bNum. Did not reuse the variables in the later half due to avoiding contamination. Addressed issue of strange behavior with the equal sign when only one number was used to enter along with equal resulting in the programming bugging out. Deleted a lot of unnecessary comments of code no longer used. Will work on backspace implementation and clicking now that keypress seems to be working. Will also dress up Math Expressions a bit more asethically.

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,4 @@
 
-//operate function, takes an operator and 2 numbers then calls one of the 4 functions
-//4 functions, add, subtract, multiply, and divide
-// const btn0 = document.getElementById('0');
-// const btn1 = document.getElementById('one');
-// const btn2 = document.getElementById('two');                        
-// const btn3 = document.getElementById('three');
-// const btn4 = document.getElementById('four');
-// const btn5 = document.getElementById('five');
-// const btn6 = document.getElementById('six');
-// const btn7 = document.getElementById('seven');
-// const btn8 = document.getElementById('eight');
-// const btn9 = document.getElementById('nine');
-
 const backspace = document.getElementById('backspace');
 const clear = document.getElementById('clear');
 const display = document.getElementById('displayScreen');
@@ -35,6 +22,13 @@ const operators = {
   '*': (a, b) => a * b,
   '/': (a, b) => a / b,
   '.': (a, b) => parseFloat(`${a}.${b}`),
+//decimal functionality
+//a . b  = a.b
+// a.b + number = a.b + number result, works.
+//a.b + a.b = a.b number does not work.  because it's getting rewritten
+
+
+
   '%': (a, b = undefined) => {
     if(b !== undefined && b !== 0){
       return b / 100;
@@ -67,7 +61,13 @@ function calculate(operator, a, b) {
   result = opFunc(a, b);
 
   console.log(`${a} ${operator} ${b} = ${result}`);
-  sDisplay.innerText = `${a} ${operator} ${b} = ${result}`;
+
+  if(operator === '%' || operator === '_'){
+    sDisplay.innerText =`${a} ${operator} = ${result}`;
+  } else {
+    sDisplay.innerText = `${a} ${operator} ${b} = ${result}`;
+  }
+ 
 
 
   display.innerText = result;
@@ -85,33 +85,58 @@ function clearAll() {
   sum = 0;
   chosenOperation = '';
   display.innerText = 0;
-  sDisplay.innerText = 0;
+  sDisplay.innerText = 'Empty';
 }
 
 clear.addEventListener('click', clearAll);
-backspace.addEventListener('click', function() {
-  // Remove the latest character from the string, issue with deleting only the first character also not permanant
-  if (result) {
-    let txt = result.toString();
-    txt = txt.includes('.') ? Math.floor(parseFloat(txt)).toString() : txt.substring(0, txt.length-1);
-    console.log(txt);
+// backspace.addEventListener('click', function() {
+//   // Remove the latest character from the string, issue with deleting only the first character also not permanant
+//   if (result) {
+//     let txt = result.toString();
+//     txt = txt.includes('.') ? Math.floor(parseFloat(txt)).toString() : txt.substring(0, txt.length-1);
+//     console.log(txt);
 
-    display.innerText = txt;
-  }
-});
+//     display.innerText = txt;
+//   }
+// });
 
 
 document.addEventListener('keydown', function (e) {
-  if (isFinite(e.key)) {
+  if (isFinite(e.key) || e.key == '.') {
     let number = e.key;
     numberHold += e.key.toString();
     display.innerText = numberHold;
 
     console.log('the number chosen is: ' + number);
+    aNumber = numberHold;
+
+
+    
+    if (e.key == '.' ) {
+      chosenOperation = '.';
+      opStorage.push(chosenOperation);
+      if(aNumber && bNumber){
+        console.log('worked ! aNumber ' + aNumber + ' bNumber ' + bNumber)
+        result = calculate(chosenOperation, aNumber, bNumber);
+        display.innerText = result;
+        return result;
+      }
+      
+      
+    bNumber = numberHold;
+    
+      // if(opStorage.length ===1 && e.key === '.' && opStorage[0] != '.'){
+      //   opStorage[1] = '.';
+      // } 
+    
+    
+    }
+   
+
 
   } else if (
     e.key == '*' || e.key == '+' || e.key == '-' || e.key == '/' ||
-    e.key == '.' || e.key == '%' || e.key == '_' ) {
+    e.key == '%' || e.key == '_' ) {
 
     display.innerText = e.key;
     chosenOperation = e.key;
@@ -127,41 +152,42 @@ document.addEventListener('keydown', function (e) {
     }
 
 
-
-
     if(storage.length === 0){
         storage[0] = Number(numberHold);
         aNumber = Number(numberHold)
         console.log(storage);
         numberHold = '';
-    }// 
-    
-    
+    } 
+
+
+
   } else if (e.key == '=') {
-    // if(storage.length === 1 && storage[0] < -1){
 
-    // }
+      if(storage.length > 2){
+        storage[1] = storage[2];
+        storage.pop();
+      } else {
 
-    if (storage.length === 0) {
-      storage[0] = 0;
-      // storage[0] = Number(numberHold);
-      // aNumber = Number(numberHold);
-    }
+      // } 
+      storage.push(Number(numberHold));
+      // bNumber = Number(numberHold) 
+       numberHold = '';
 
-      storage[1] = Number(numberHold);
-      bNumber = Number(numberHold) 
-      numberHold = '';
-
-      //somewhere have the decimal and percentage here
-    if(storage.length === 2 && opStorage.length === 2 ){
-      if(opStorage[1] === '_'){
-        sum = calculate(opStorage[1], Number(storage[1]), 0)
-      } else if( opStorage[1] === '%'){
-        sum = calculate(opStorage[1], Number(storage[1]), 0)
       }
 
+
+    if(storage.length === 2 && opStorage.length === 2 ){
+      if(opStorage[1] === '_'){
+        sum = calculate(opStorage[1], Number(storage[1]), 0);
+        result = calculate(opStorage[0], Number(storage[0]), sum);
+      } else if( opStorage[1] === '%'){
+        sum = calculate(opStorage[1], Number(storage[1]), 0);
+        result = calculate(opStorage[0], Number(storage[0]), sum);
+      } else if( opStorage[1] === '.'){
+        result = calculate(opStorage[0], Number(storage[0]), Number(storage[1]));
+      }
       
-      result = calculate(opStorage[0], Number(storage[0]), sum)
+      
       storage = [result];
       opStorage = [];
       return (display.innerText = result);
@@ -169,27 +195,33 @@ document.addEventListener('keydown', function (e) {
 
 
     
-    } else if (storage.length === 2 && opStorage.length === 1) { 
+    } else if (storage.length === 2 && opStorage.length === 1 && opStorage[0] !== '.') { 
       result = calculate(opStorage[0], Number(storage[0]), Number(storage[1]));
       storage = [result];
       opStorage = [];
       return (display.innerText = result);
-    } else if (storage.length === 2){
+    } else if (storage.length === 2 && opStorage[0] == '='){
       result = Number(storage[0])
       storage = [result];
       opStorage = [];
       return (display.innerText = result);
-    } else {
-      console.log('please enter Number + operator + number before entering');
+    } else{
+      //this was created due to people entering number by itself then clicking equals.
+      if(storage.length === 1 && opStorage.length === 0){
+        result =  storage[0];
+        storage = [result]
+      }
+      // if(opStorage.length === 0){
+      //   console.log('please enter Number + operator + number before pressing equal');
+      //   sDisplay.innerText = 'Please enter Number + Operator + Number before pressing Equal!';
+      // } else {
+      //   console.log('calculating result: ' + result)
+      //   sDisplay.innerText = `calculating result:   ${result}`;
+      //   storage = [result]
+      // }
+      
     }
   } 
-    //   else {
-    //   storage[1] = Number(numberHold);
-    //   numberHold = '';
-    //   console.log(storage);
-    // }
-
-
 });
 
   
